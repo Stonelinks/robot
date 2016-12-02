bdb189c81865e96d803e611c35a4ce0704cff00d
Lucas Doyle <ldoyle@airware.com>
2016-12-02 00:38:33 -0800

    Add standard for linting

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,39 @@
-import React, { Component } from 'react';
-import './App.css';
+/* eslint-env browser */
+import React, { Component } from 'react'
+import './App.css'
 
 import Broadway from 'broadwayjs'
 
 class WebCam extends Component {
 
-  componentDidMount() {
+  componentDidMount () {
     this.initialize()
   }
 
-  initialize() {
-    const client = new WebSocket(this.props.address);
-    client.binaryType = 'arraybuffer';
+  initialize () {
+    const client = new WebSocket(this.props.address)
+    client.binaryType = 'arraybuffer'
 
     const decoderURL = (process.env.REACT_APP_BROADWAY_PLAYER_WORKER ? window.location.protocol + '//' + window.location.hostname + process.env.REACT_APP_BROADWAY_PLAYER_WORKER : '/Decoder.js')
-    var response;
+    var response
 
-    var get = new XMLHttpRequest();
-    get.open("GET", decoderURL, false);
+    var get = new XMLHttpRequest()
+    get.open('GET', decoderURL, false)
     get.onreadystatechange = function () {
-      if (get.readyState == 4 && get.status == 200) {
-        response = 'var global = {};\n' + get.responseText;
+      if (get.readyState === 4 && get.status === 200) {
+        response = 'var global = {};\n' + get.responseText
       }
     }
-    get.send();
+    get.send()
 
-    var blob;
+    var blob
     try {
-      blob = new Blob([response], { type: 'application/javascript' });
+      blob = new Blob([response], { type: 'application/javascript' })
     } catch (e) { // Backwards-compatibility
-      window.BlobBuilder = window.BlobBuilder || window.WebKitBlobBuilder || window.MozBlobBuilder;
+      window.BlobBuilder = window.BlobBuilder || window.WebKitBlobBuilder || window.MozBlobBuilder
       blob = new BlobBuilder(); // eslint-disable-line
-      blob.append(response);
-      blob = blob.getBlob();
+      blob.append(response)
+      blob = blob.getBlob()
     }
 
     var p = new Broadway.Player({
@@ -43,7 +44,7 @@ class WebCam extends Component {
         height: this.props.height,
         width: this.props.width
       }
-    });
+    })
 
     this.container.appendChild(p.canvas)
 
@@ -51,14 +52,14 @@ class WebCam extends Component {
       const frame = new Uint8Array(e.data)
       var naltype
       if (frame.length > 4) {
-        if (frame[4] == 0x65) {
-          naltype = "I frame";
-        } else if (frame[4] == 0x41) {
-          naltype = "P frame";
-        } else if (frame[4] == 0x67) {
-          naltype = "SPS";
-        } else if (frame[4] == 0x68) {
-          naltype = "PPS";
+        if (frame[4] === 0x65) {
+          naltype = 'I frame'
+        } else if (frame[4] === 0x41) {
+          naltype = 'P frame'
+        } else if (frame[4] === 0x67) {
+          naltype = 'SPS'
+        } else if (frame[4] === 0x68) {
+          naltype = 'PPS'
         }
       }
       // console.log(naltype);
@@ -66,25 +67,22 @@ class WebCam extends Component {
     }
 
     client.addEventListener('close', function () {
-      console.log('Got close event, reconnecting');
+      console.log('Got close event, reconnecting')
       // player.stop()
-      setTimeout(this.initialize, 1000);
-    });
+      setTimeout(this.initialize, 1000)
+    })
   }
-  render() {
-    return ( <
-      div ref = {
-        (c) => this.container = c }
-      height = { this.props.height }
-      width = { this.props.width }
-      style = {
-        {
-          height: `${this.props.height}px`,
-          width: `${this.props.width}px`,
-          margin: '0 auto'
-        }
-      }
-      />
+  render () {
+    return (<
+      div ref={
+        (c) => this.container = c}
+      height={this.props.height}
+      width={this.props.width}
+      style={{
+        height: `${this.props.height}px`,
+        width: `${this.props.width}px`,
+        margin: '0 auto'
+      }} />
     )
   }
 }
@@ -93,23 +91,24 @@ WebCam.defaultProps = {
   address: null,
   width: 320 * 2,
   height: 240 * 2
-};
+}
+
 class App extends Component {
-  render() {
+  render () {
     const websocketAddress = (location.protocol === 'https:' ? 'wss' : 'ws') + '://' + (process.env.REACT_APP_WSS_PORT ? window.location.hostname + ':' + process.env.REACT_APP_WSS_PORT : window.location.host)
-    console.log(`websocket address: ${websocketAddress}`);
+    console.log(`websocket address: ${websocketAddress}`)
 
     return (
-      <div className="App">
-        <div className="App-header">
+      <div className='App'>
+        <div className='App-header'>
           <h2>Luke's Robot</h2>
         </div>
-        <div className="App-intro">
-          <WebCam address={websocketAddress}/>
+        <div className='App-intro'>
+          <WebCam address={websocketAddress} />
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
