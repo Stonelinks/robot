c1409f22395820f5f8b556ccc7fd9bf5020a8144
Lucas Doyle <lucas.doyle@samsara.com>
2021-11-07 19:15:08 -0800

    get rid of RTSP dependency

--- device/config.h
+++ device/config.h
@@ -1,33 +1,15 @@
-/***************************************
- *  PinOUT
- **************************************/
-#define PWDN_GPIO_NUM 26
-#define RESET_GPIO_NUM -1
-#define XCLK_GPIO_NUM 32
-#define SIOD_GPIO_NUM 13
-#define SIOC_GPIO_NUM 12
-
-#define Y9_GPIO_NUM 39
-#define Y8_GPIO_NUM 36
-#define Y7_GPIO_NUM 23
-#define Y6_GPIO_NUM 18
-#define Y5_GPIO_NUM 15
-#define Y4_GPIO_NUM 4
-#define Y3_GPIO_NUM 14
-#define Y2_GPIO_NUM 5
-#define VSYNC_GPIO_NUM 27
-#define HREF_GPIO_NUM 25
-#define PCLK_GPIO_NUM 19
-#define AS312_PIN 33
-#define BUTTON_1 34
-
+#define SSD130_MODLE_TYPE GEOMETRY_128_64
 #define I2C_SDA 21
 #define I2C_SCL 22
-
-#define SSD130_MODLE_TYPE GEOMETRY_128_64
-
 #include "OLEDDisplayUi.h"
 #include "SSD1306.h"
 #define SSD1306_ADDRESS 0x3c
 
 // #define ENABLE_BUTTON
+#define BUTTON_1 34
+
+#define ENABLE_OTA
+#define OTA_PORT 8269
+
+#define ENABLE_WEBSERVER
+#define WEBSERVER_PORT 80
--- device/device.ino
+++ device/device.ino
@@ -1,14 +1,14 @@
-#include "esp_camera.h"
-#include "esp_wifi.h"
 #include "freertos/event_groups.h"
 #include <ArduinoOTA.h>
 #include <OneButton.h>
-#include <WiFi.h>
+#include <WebServer.h>
 #include <Wire.h>
 
-#include "CRtspSession.h"
+#include <WebServer.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+
 #include "OV2640.h"
-#include "OV2640Streamer.h"
 
 #include "config.h"
 
@@ -26,6 +26,8 @@ String ssid;
 
 EventGroupHandle_t evGroup;
 
+OV2640 cam;
+
 SSD1306 oled(SSD1306_ADDRESS, I2C_SDA, I2C_SCL, SSD130_MODLE_TYPE);
 OLEDDisplayUi ui(&oled);
 
@@ -34,12 +36,74 @@ bool en = false;
 OneButton button1(BUTTON_1, true);
 #endif
 
+#ifdef ENABLE_WEBSERVER
+WebServer server(WEBSERVER_PORT);
+#endif
+
+#ifdef ENABLE_WEBSERVER
+void handle_jpg_stream(void) {
+  WiFiClient client = server.client();
+  String response = "HTTP/1.1 200 OK\r\n";
+  response += "Content-Type: multipart/x-mixed-replace; boundary=frame\r\n\r\n";
+  server.sendContent(response);
+
+  while (1) {
+    cam.run();
+    if (!client.connected())
+      break;
+    response = "--frame\r\n";
+    response += "Content-Type: image/jpeg\r\n\r\n";
+    server.sendContent(response);
+
+    client.write((char *)cam.getfb(), cam.getSize());
+    server.sendContent("\r\n");
+    if (!client.connected())
+      break;
+  }
+}
+
+void handle_jpg(void) {
+  WiFiClient client = server.client();
+
+  cam.run();
+  if (!client.connected()) {
+    return;
+  }
+  String response = "HTTP/1.1 200 OK\r\n";
+  response += "Content-disposition: inline; filename=capture.jpg\r\n";
+  response += "Content-type: image/jpeg\r\n\r\n";
+  server.sendContent(response);
+  client.write((char *)cam.getfb(), cam.getSize());
+}
+
+void handleNotFound() {
+  String message = "Server is running!\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  server.send(200, "text/plain", message);
+}
+#endif
+
 void initNet() {
+  connectToWifi();
+
   // Hostname
   uint64_t chipid = ESP.getEfuseMac(); // The chip ID is essentially its MAC
                                        // address(length: 6 bytes).
   uint16_t chip = (uint16_t)(chipid >> 32);
   sprintf(device_name, "ldcam-%04X%08X", chip, (uint32_t)chipid);
+
+#ifdef ENABLE_WEBSERVER
+  server.on("/mjpeg", HTTP_GET, handle_jpg_stream);
+  server.on("/snapshot", HTTP_GET, handle_jpg);
+  server.onNotFound(handleNotFound);
+  server.begin();
+#endif
 }
 
 void connectToWifi() {
@@ -168,74 +232,13 @@ void uiDrawString(int x_offset, int y_offset, String message) {
   Serial.println(message);
 }
 
-void startCameraServer();
+// void startCameraServer();
 
-void setup() {
-  // Serial.begin(115200);
-  Serial.begin(9600);
-  Serial.setDebugOutput(true);
-  Serial.println();
-
-  if (I2C_SDA > 0) {
-    Wire.begin(I2C_SDA, I2C_SCL);
-  }
-
-  initUi();
-  initNet();
-
-  camera_config_t config;
-  config.ledc_channel = LEDC_CHANNEL_0;
-  config.ledc_timer = LEDC_TIMER_0;
-  config.pin_d0 = Y2_GPIO_NUM;
-  config.pin_d1 = Y3_GPIO_NUM;
-  config.pin_d2 = Y4_GPIO_NUM;
-  config.pin_d3 = Y5_GPIO_NUM;
-  config.pin_d4 = Y6_GPIO_NUM;
-  config.pin_d5 = Y7_GPIO_NUM;
-  config.pin_d6 = Y8_GPIO_NUM;
-  config.pin_d7 = Y9_GPIO_NUM;
-  config.pin_xclk = XCLK_GPIO_NUM;
-  config.pin_pclk = PCLK_GPIO_NUM;
-  config.pin_vsync = VSYNC_GPIO_NUM;
-  config.pin_href = HREF_GPIO_NUM;
-  config.pin_sscb_sda = SIOD_GPIO_NUM;
-  config.pin_sscb_scl = SIOC_GPIO_NUM;
-  config.pin_pwdn = PWDN_GPIO_NUM;
-  config.pin_reset = RESET_GPIO_NUM;
-  config.xclk_freq_hz = 20000000;
-  config.pixel_format = PIXFORMAT_JPEG;
-
-  // init with high specs to pre-allocate larger buffers
-  // if (psramFound()) {
-  //   config.frame_size = FRAMESIZE_UXGA;
-  //   config.jpeg_quality = 10;
-  //   config.fb_count = 2;
-  // } else {
-  config.frame_size = FRAMESIZE_SVGA;
-  config.jpeg_quality = 12;
-  config.fb_count = 1;
-  // }
-
-  esp_err_t err = esp_camera_init(&config);
-  if (err != ESP_OK) {
-    uiDrawString(0, 0, "Camera init Fail");
-    while (1)
-      ;
-  }
-
-  sensor_t *s = esp_camera_sensor_get();
-  s->set_vflip(s, 1);
-
-  connectToWifi();
-
+void initOta() {
   // Port defaults to 8266
-  ArduinoOTA.setPort(8269);
+  ArduinoOTA.setPort(OTA_PORT);
 
-  // ArduinoOTA.setHostname(device_name);
-
-  // No authentication by default
-  // ArduinoOTA.setPassword((const char *)"123");
-
+  ArduinoOTA.setHostname(device_name);
   ArduinoOTA.onStart([]() { uiDrawString(0, 0, "OTA: Start"); });
   ArduinoOTA.onEnd([]() { uiDrawString(0, 0, "OTA: End"); });
   ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
@@ -261,7 +264,41 @@ void setup() {
   });
 
   ArduinoOTA.begin();
-  startCameraServer();
+}
+
+void initCam() {
+  camera_config_t config = esp32cam_ttgo_t_config;
+
+  esp_err_t err = cam.init(config);
+  if (err != ESP_OK) {
+    uiDrawString(0, 0, "Camera init Fail");
+    while (1)
+      ;
+  }
+
+  sensor_t *s = esp_camera_sensor_get();
+  s->set_vflip(s, 1);
+}
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial) {
+    ;
+  }
+  Serial.println();
+
+  if (I2C_SDA > 0) {
+    Wire.begin(I2C_SDA, I2C_SCL);
+  }
+
+  initUi();
+  initCam();
+
+  initNet();
+
+#ifdef ENABLE_OTA
+  initOta();
+#endif
 
   Serial.print("Camera Ready! Use 'http://");
   Serial.print(ip);
@@ -276,5 +313,12 @@ void loop() {
     delay(500);
 #endif
   }
+
+#ifdef ENABLE_OTA
   ArduinoOTA.handle();
+#endif
+
+#ifdef ENABLE_WEBSERVER
+  server.handleClient();
+#endif
 }
